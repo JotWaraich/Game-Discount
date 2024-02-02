c4ac976c534230f13ff4801f3fb13e24b52d5574
Jot Waraich <gurjagjot@gmail.com>
2024-02-02 14:07:56 -0700

    some morevisual improvements

--- app/Components/Card.jsx
+++ app/Components/Card.jsx
@@ -42,6 +42,7 @@ const Card = ({ title, salePrice, imgURL, normalPrice, rating }) => {
           <a
             href="#"
             className="mb-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            id="add-to-cart"
           >
             Add to cart
           </a>
